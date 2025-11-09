49e92c3a9a85de650e9870b661a923e7f67825a2
JaniruShehan <janirushehan20@gmail.com>
2025-11-09 21:34:39 +0530

    Added First Java File.

---
+++ Java Exercises/Exercise1.java
@@ -0,0 +1,21 @@
+public class Exercise1 {
+    public static void main(String[] args){
+
+        //THIS IS MY FIRST JAVA PROJECT.
+
+        /*
+         This is 
+         a multi-line 
+         comment.
+         */
+
+        System.out.println("I like Pizza !");
+        System.out.println("I like Hot Dog !");
+        System.out.println("I like Burger !");
+
+        System.out.print("I like Pizza !\n");
+        System.out.print("I like Hot Dog !\n");
+        System.out.print("I like Burger !");
+    }
+    
+}
