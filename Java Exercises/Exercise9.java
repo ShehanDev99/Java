b11fc39caa5277ffd1070ca8dbd8931ea637eebc
JaniruShehan <janirushehan20@gmail.com>
2025-11-15 13:09:58 +0530

    Math Class In Java.

---
+++ Java Exercises/Exercise9.java
@@ -0,0 +1,36 @@
+public class Exercise9 {
+    //Math Class in Java
+    public static void main(String[] args){
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        //Absolute Value
+        System.out.println(Math.abs(-10));
+        //Power
+        System.out.println(Math.pow(2,3));
+        //Square Root
+        System.out.println(Math.sqrt(4));
+        //Ceiling
+        System.out.println(Math.ceil(3.56));
+        //Floor
+        System.out.println(Math.floor(2.12));
+        //Round
+        System.out.println(Math.round(5.65));
+        //Max
+        System.out.println(Math.max(10,8));
+        //Min
+        System.out.println(Math.min(5,6));
+
+
+        //Complex Expression
+        System.out.println(Math.sqrt(Math.pow(2,Math.max(2,Math.abs(-4)))));
+
+
+
+
+
+
+    }
+    
+}
